607dceaa3509964fdc62750418391e25aa79476c
Daniel Co <dcohomemail@gmail.com>
2021-05-24 10:29:25 -0700

    lint fixes

--- src/components/RestaurantScreen.js
+++ src/components/RestaurantScreen.js
@@ -1,10 +1,10 @@
 import RestaurantList from './RestaurantList';
 
 const RestaurantScreen = () => (
-  <div>
-    <h1>Restaurants</h1>
-    <RestaurantList />
-  </div>
+    <div>
+        <h1>Restaurants</h1>
+        <RestaurantList />
+    </div>
 );
 
 export default RestaurantScreen;
